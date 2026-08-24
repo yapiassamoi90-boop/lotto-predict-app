ee810316c5c48bc15e13ed47882e915182bd6e8e
yapiassamoi90-boop <yapiassamoi90@gmail.com>
2026-08-24 08:29:56 +0000

    Add service worker for asset caching

---
+++ Sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'lotto-pwa-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+
